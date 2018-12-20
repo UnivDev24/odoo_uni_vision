e10277d6d21aec0a06ab7863ef1cace38332ad37
UnivDev24 <42786532+UnivDev24@users.noreply.github.com>
2018-12-20 23:21:16 +0000

    modification manifest for univision reports

--- odoo/addons/univision_reports/__manifest__.py
+++ odoo/addons/univision_reports/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['account_invoicing, account_accountant, sale_management'],
+    'depends': ['account, web, sale_management'],
 
     # always loaded
     'data': [
