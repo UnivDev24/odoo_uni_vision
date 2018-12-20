8d0aa9ccaab26b874780d344f719661246ab3489
UnivDev24 <42786532+UnivDev24@users.noreply.github.com>
2018-12-20 21:28:45 +0000

    add module univision containing reports personnalisation

---
+++ odoo/addons/univision_reports/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "univision_reports",
+
+    'summary': """
+        module containing reports personnalisation for uni vision""",
+
+    'description': """
+        personnalisation for invoice / headers / etc
+    """,
+
+    'author': "Uni Vision",
+    'website': "http://uni-vision.fr/",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'reports',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['account'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        #'views/views.xml',
+        'views/tpl_report_invoice_univision.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
