268ed49f1e22b45cf5167eb67ae364d0feb63e6c
UnivDev24 <42786532+UnivDev24@users.noreply.github.com>
2018-12-20 22:30:52 +0000

    modify manifest reports

--- odoo/addons/univision_reports/__manifest__.py
+++ odoo/addons/univision_reports/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['account, web'],
+    'depends': ['account, web, sale'],
 
     # always loaded
     'data': [
