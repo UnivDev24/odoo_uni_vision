51f3ab02284485fa42435dc5d0fc5739707c8b1d
UnivDev24 <felicie.fillaudeau@googlemail.com>
2022-02-12 14:38:54 +0000

    add module to customize univision theme

---
+++ odoo/addons/theme_univision/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Univision theme',
+    'description': 'Contains the theme of UniVision.',
+    'version': '1.0',
+    'author': "Uni Vision",
+    'website': "http://uni-vision.fr/",
+    'license':"LGPL-3",
+    'category': 'Theme/Creative',
+
+    'depends': ['website', 'website_theme_install'],
+    'data': [
+    ],
+}
