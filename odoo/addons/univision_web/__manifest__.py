5a49a1dc5c281398d32d3ddfb56b7a885187290b
UnivDev24 <felicie.fillaudeau@googlemail.com>
2023-07-04 15:16:54 +0200

    NEO-19 : modify maniest univision_web

--- odoo/addons/univision_web/__manifest__.py
+++ odoo/addons/univision_web/__manifest__.py
@@ -11,25 +11,25 @@
 
     'author': "Uni Vision",
     'website': "http://uni-vision.fr/",
-    'license':"LGPL-3",
+    'license': "LGPL-3",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'web',
+    'category': 'Web/Theme',
     'version': '0.2',
-    'assets' : {
-        'web.report_assets_common': [            
-            'univision_web/static/scss/report.scss',
-            'univision_web/static/scss/layout_background.scss'
+    'assets': {
+        'web.report_assets_common': [
+            ('include', 'univision_web/static/src/scss/layout_background.scss'),
+            ('include', 'univision_web/static/src/scss/report.scss')
         ],
     },
     # any module necessary for this one to work correctly
     'depends': ['web'],
 
     # always loaded
-    'data': [        
-       'views/report_templates.xml',
+    'data': [
+        'views/report_templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
