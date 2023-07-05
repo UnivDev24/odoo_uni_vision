29b22efd58f0ffb34213b249bbb48ee8cf0cbba9
UnivDev24 <felicie.fillaudeau@googlemail.com>
2023-07-05 15:34:38 +0200

    NEO-19 : delete views and data in premigration for module univision_contacts

--- odoo/addons/univision_contacts/migrations/16.0.1.0.0/pre-migration.py
+++ odoo/addons/univision_contacts/migrations/16.0.1.0.0/pre-migration.py
@@ -6,13 +6,14 @@ _logger = logging.getLogger(__name__)
 def migrate(cr, version):
     _logger.info('univision_contacts : pre-migration start')
 
-    # inactive old view
+    # REMOVE ALL VIEWS THAT ARE NOT IN MODULES
     cr.execute("""
-            update ir_ui_view v
-            set inherit_id = NULL, active = false
-            where
-            v.id in (1156)
-        """)
+            DELETE FROM ir_ui_view WHERE id=1156
+    """)
 
+    # REMOVE VIEWS THAT ARE NOT USED ANYMORE
+    cr.execute("""
+            DELETE FROM ir_model_data WHERE id=70413
+    """)
 
     _logger.info('univision_contacts : pre-migration end')
