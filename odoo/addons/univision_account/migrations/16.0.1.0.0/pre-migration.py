b282c246fa42cada1d35f11644ccf22fc821e399
UnivDev24 <felicie.fillaudeau@googlemail.com>
2023-07-05 16:08:19 +0200

    NEO-19 : update

--- odoo/addons/univision_account/migrations/16.0.1.0.0/pre-migration.py
+++ odoo/addons/univision_account/migrations/16.0.1.0.0/pre-migration.py
@@ -5,15 +5,6 @@ def migrate(cr, version):
 
     _logger.info('UniVision Account : pre-migration start')
 
-    # REMOVE ALL VIEWS THAT ARE NOT IN MODULES
-    # cr.execute("""
-    #                DELETE FROM ir_actions_report WHERE id=419
-    #        """)
-    #
-    # cr.execute("""
-    #                    DELETE FROM ir_actions_report WHERE id=420
-    #            """)
-
     # REMOVE VIEWS THAT ARE NOT USED ANYMORE
     cr.execute("""
                        DELETE FROM ir_model_data WHERE id=34756
--- odoo/addons/univision_sale/migrations/16.0.1.0.0/pre-migration.py
+++ odoo/addons/univision_sale/migrations/16.0.1.0.0/pre-migration.py
@@ -6,15 +6,6 @@ _logger = logging.getLogger(__name__)
 def migrate(cr, version):
     _logger.info('univision_sale : pre-migration start')
 
-    # REMOVE ALL VIEWS THAT ARE NOT IN MODULES
-    # cr.execute("""
-    #            DELETE FROM ir_actions_report WHERE id=421
-    #    """)
-    #
-    # cr.execute("""
-    #                DELETE FROM ir_actions_report WHERE id=422
-    #        """)
-
     # REMOVE VIEWS THAT ARE NOT USED ANYMORE
     cr.execute("""
                    DELETE FROM ir_model_data WHERE id=34759
