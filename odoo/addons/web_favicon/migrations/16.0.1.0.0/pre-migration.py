fdfb582d265e36b4a5ea37c081fd4b1724c6328d
UnivDev24 <felicie.fillaudeau@googlemail.com>
2023-07-07 13:22:14 +0200

    NEO-19 : delete view and data in pre migration favicon

--- odoo/addons/web_favicon/migrations/16.0.1.0.0/pre-migration.py
+++ odoo/addons/web_favicon/migrations/16.0.1.0.0/pre-migration.py
@@ -5,4 +5,37 @@ def migrate(cr, version):
 
     _logger.info('Web favicon : pre-migration start')
 
+    cr.execute("""
+               DELETE FROM ir_model_data WHERE id=69260
+       """)
+
+    cr.execute("""
+                   DELETE FROM ir_model_data WHERE id=69257
+           """)
+
+    cr.execute("""
+                       DELETE FROM ir_model_data WHERE id=69258
+               """)
+
+    cr.execute("""
+                           DELETE FROM ir_model_data WHERE id=69259
+                   """)
+
+    cr.execute("""
+                       DELETE FROM ir_ui_view WHERE id=1149
+               """)
+
+    cr.execute("""
+                   DELETE FROM ir_ui_view WHERE id=1150
+           """)
+
+    cr.execute("""
+                      DELETE FROM ir_model_fields WHERE id=9868
+              """)
+
+    cr.execute("""
+                          DELETE FROM ir_model_fields WHERE id=9869
+                  """)
+
+
     _logger.info('Web favicon : pre-migration end')
