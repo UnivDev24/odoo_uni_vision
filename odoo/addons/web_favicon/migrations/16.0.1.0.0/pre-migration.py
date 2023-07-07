023cba17433a98c9d0d85448283920090a27380a
UnivDev24 <felicie.fillaudeau@googlemail.com>
2023-07-07 12:55:08 +0200

    NEO-19 : try to add missing migration for oca/web unupgraded modules

---
+++ odoo/addons/web_favicon/migrations/16.0.1.0.0/pre-migration.py
@@ -0,0 +1,8 @@
+import logging
+_logger = logging.getLogger(__name__)
+
+def migrate(cr, version):
+
+    _logger.info('Web favicon : pre-migration start')
+
+    _logger.info('Web favicon : pre-migration end')
---
+++ odoo/addons/web_listview_range_select/migrations/16.0.1.0.0/pre-migration.py
@@ -0,0 +1,8 @@
+import logging
+_logger = logging.getLogger(__name__)
+
+def migrate(cr, version):
+
+    _logger.info('Web list view range : pre-migration start')
+
+    _logger.info('Web list view range : pre-migration end')
