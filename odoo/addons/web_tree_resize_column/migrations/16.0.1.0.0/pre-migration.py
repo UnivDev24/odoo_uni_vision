5f10067c3b32b17122684f7e8b31de8f13a95974
UnivDev24 <felicie.fillaudeau@googlemail.com>
2023-09-09 15:40:27 +0200

    NEO-19 : remove OCA directories

--- odoo/addons/web_tree_resize_column/migrations/16.0.1.0.0/pre-migration.py
+++
@@ -1,8 +0,0 @@
-import logging
-_logger = logging.getLogger(__name__)
-
-def migrate(cr, version):
-
-    _logger.info('Web tree resize column : pre-migration start')
-
-    _logger.info('Web tree resize column : pre-migration end')
--- odoo/addons/web_widget_many2many_tags_multi_selection/migrations/16.0.1.0.0/pre-migration.py
+++
@@ -1,8 +0,0 @@
-import logging
-_logger = logging.getLogger(__name__)
-
-def migrate(cr, version):
-
-    _logger.info('Web widget many2many tags multi selection : pre-migration start')
-
-    _logger.info('Web widget many2many tags multi selection : pre-migration end')
