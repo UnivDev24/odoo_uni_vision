e24507861fd1c8fb98262bdd9f16ababca7bb7b5
UnivDev24 <felicie.fillaudeau@googlemail.com>
2023-09-09 17:37:43 +0200

    NEO-19 : add model for custom module univision_contact

--- odoo/addons/univision_contacts/models/models.py
+++ odoo/addons/univision_contacts/models/models.py
@@ -8,40 +8,40 @@ from odoo import models, fields, api
 #we inherit res_partner to modify the default value of customer
 
 # FIXME LATER
-# class univision_contacts(models.Model):
-#     _inherit ='res.partner'
-#
-#
-#     def open_record(self):
-#         return {'type': 'ir.actions.act_window',
-#                 'res_model': 'res.partner',
-#                 'name': 'Contacts',
-#                 'view_type': 'form',
-#                 'view_mode': 'form',
-#                 'res_id': self.id,
-#                 'target': 'current'}
-#
-#     def action_open_new_tab(self):
-#
-#         for rec in self:
-#
-#             base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
-#
-#
-#             menu = self.env.ref('contacts.menu_contacts')
-#             action = self.env.ref('contacts.action_contacts')
-#
-#             record_url = base_url + "/web#id=" + str(self.id) + "&action=" + str(action.id) + "&model=res.partner&view_type=form&menu_id=" + str(menu.id)
-#
-#             client_action = {
-#
-#                 'type': 'ir.actions.act_url',
-#
-#                 'name': "Contact",
-#
-#
-#                 'target': 'new',
-#
-#                 'url': record_url}
-#
-#         return client_action
+class univision_contacts(models.Model):
+    _inherit ='res.partner'
+
+
+    def open_record(self):
+        return {'type': 'ir.actions.act_window',
+                'res_model': 'res.partner',
+                'name': 'Contacts',
+                'view_type': 'form',
+                'view_mode': 'form',
+                'res_id': self.id,
+                'target': 'current'}
+
+    def action_open_new_tab(self):
+
+        for rec in self:
+
+            base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+
+
+            menu = self.env.ref('contacts.menu_contacts')
+            action = self.env.ref('contacts.action_contacts')
+
+            record_url = base_url + "/web#id=" + str(self.id) + "&action=" + str(action.id) + "&model=res.partner&view_type=form&menu_id=" + str(menu.id)
+
+            client_action = {
+
+                'type': 'ir.actions.act_url',
+
+                'name': "Contact",
+
+
+                'target': 'new',
+
+                'url': record_url}
+
+        return client_action
