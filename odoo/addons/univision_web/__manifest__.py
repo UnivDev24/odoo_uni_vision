d417476d4963ae97824170f4826535e8ca3adb02
UnivDev24 <felicie.fillaudeau@googlemail.com>
2023-09-16 17:51:16 +0200

    NEO-19 : update asset

--- odoo/addons/univision_web/__manifest__.py
+++ odoo/addons/univision_web/__manifest__.py
@@ -20,12 +20,12 @@
     'version': '16.0.1.0.0',
     'assets': {
         'web.assets_common': [
-            'static/src/scss/layout_background.scss',
-            'static/src/scss/report.scss',
+            'univision_web/static/src/scss/layout_background.scss',
+            'univision_web/static/src/scss/report.scss',
             ],
         'web.reports_assets_common': [
-            'static/src/scss/layout_background.scss',
-            'static/src/scss/report.scss',
+            'univision_web/static/src/scss/layout_background.scss',
+            'univision_web/static/src/scss/report.scss',
         ],
     },
     # any module necessary for this one to work correctly
