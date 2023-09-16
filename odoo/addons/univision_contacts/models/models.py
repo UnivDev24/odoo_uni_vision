0bc781909f87c5901db35d179cb21e435c90e2f3
UnivDev24 <felicie.fillaudeau@googlemail.com>
2023-09-16 15:18:31 +0200

    NEO-19 : correction import models

--- odoo/addons/univision_contacts/models/models.py
+++ odoo/addons/univision_contacts/models/models.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
-
-# FIXMEMIG
-
-#we inherit res_partner to modify the default value of customer
-
-# FIXME LATER
+# we inherit res_partner to modify the default value of customer
 class univision_contacts(models.Model):
-    _inherit ='res.partner'
-
+    _inherit = 'res.partner'
 
     def open_record(self):
         return {'type': 'ir.actions.act_window',
@@ -22,23 +16,20 @@ class univision_contacts(models.Model):
                 'target': 'current'}
 
     def action_open_new_tab(self):
-
         for rec in self:
-
             base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
-
 
             menu = self.env.ref('contacts.menu_contacts')
             action = self.env.ref('contacts.action_contacts')
 
-            record_url = base_url + "/web#id=" + str(self.id) + "&action=" + str(action.id) + "&model=res.partner&view_type=form&menu_id=" + str(menu.id)
+            record_url = base_url + "/web#id=" + str(self.id) + "&action=" + str(
+                action.id) + "&model=res.partner&view_type=form&menu_id=" + str(menu.id)
 
             client_action = {
 
                 'type': 'ir.actions.act_url',
 
                 'name': "Contact",
-
 
                 'target': 'new',
 
