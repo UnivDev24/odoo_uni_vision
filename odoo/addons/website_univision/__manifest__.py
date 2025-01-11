300a607543fb1b1cdaed7cefd249e9479c3635d6
UnivDev24 <felicie.fillaudeau@googlemail.fr>
2025-01-11 13:58:51 +0100

    NEO-93 : remove all custom personalisation

--- odoo/addons/website_univision/__manifest__.py
+++ odoo/addons/website_univision/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Univision Theme',
     'description': 'Contains the theme of UniVision.',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.1.0',
     'author': "Uni Vision",
     'website': "http://uni-vision.fr/",
     'license': "LGPL-3",
